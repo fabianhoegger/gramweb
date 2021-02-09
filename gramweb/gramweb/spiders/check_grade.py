8e3db5d5117445e23e98a5fba586ed22d36bb0a8
Fabian <draizfabian@gmail.com>
2021-02-09 20:21:30 +0200

    logging in

--- gramweb/gramweb/spiders/check_grade.py
+++ gramweb/gramweb/spiders/check_grade.py
@@ -5,18 +5,20 @@ class CheckGradeSpider(scrapy.Spider):
     name = 'check_grade'
     allowed_domains = ['gram-web.ionio.gr']
     start_urls = ['http://gram-web.ionio.gr/unistudent/login.asp']
-    def start_requests(self):
-        return [scrapy.FormRequest("http://gram-web.ionio.gr/unistudent/login.asp",
-                cookies={'login':'True','_ga':'GA1.2.907918764.1611845502', 'ASPSESSIONIDASDCSDSS':'DNDGNMPAKHPPFHIOLOPLIKED',
-                '_gid':'GA1.2.1639919294.1612876337'})]
+    def authentication_failed(response):
+    # TODO: Check the contents of the response and return True if it failed
+    # or False if it succeeded.
+        pass
 
     def parse(self, response):
-        return {'text':response.status}
-
-"""    def make_requests_from_url(self,url):
-        request=super(CheckGradeSpider,self).make_requests_from_url(url)
-        request.cookies['login']='True'
-        request.cookies['_ga']='GA1.2.907918764.1611845502'
-        request.cookies['ASPSESSIONIDASDCSDSS']='DNDGNMPAKHPPFHIOLOPLIKED'
-        request.cookies['_gid']='GA1.2.1639919294.1612876337'
-        return request"""
+        return scrapy.FormRequest.from_response(response,
+            formdata={'userName': 'p16chai', 'pwd': 't1SCwT6t','submit1':'%C5%DF%F3%EF%E4%EF%F2','loginTrue':'login',
+            'c02c71ae0d90143449b5882db85ca536c':'E98D85C6167EE8468D7BA9E0CAE05836D923D5D392C0BCD00A27A937AB379FDA2454E0E6C2D45F2F6FE8260078500E7DAC28FDE1A11682311BD68CC918B560900169729911FEB9C1987F077382287BEBA1626F2106E0D1199F4674ADC938DE44'},
+            callback=self.after_login
+        )
+    def after_login(self,response):
+        if authentication_failed(response):
+            self.logger.error("Login failed")
+            return
+        else:
+            return {'status':response.status,'title':response.xpath("//span[@class='title']/text()").get()}
